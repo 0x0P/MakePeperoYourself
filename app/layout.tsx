a7c7c4d96aa8ecdc7527268502bf2a81ff57bf41
PERCENTZERO <percentzero@outlook.com>
2024-11-11 14:06:51 +0900

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "빼빼로메이커",
+  description: "나만의 특별한 빼빼로를 만들어봐요",
+};
 export default function RootLayout({
   children,
 }: Readonly<{
